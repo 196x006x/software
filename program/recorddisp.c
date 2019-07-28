639b232103b9732c3df6fb9aa01e9fdce0394468
Shusuke Tokutomi <shusuketokutomi@Shusukes-macbookpro.local>
2019-07-29 08:49:58 +0900

    add like a template

--- program/recorddisp.c
+++ program/recorddisp.c
@@ -1,5 +1,15 @@
 #include "liquorSystem.h"
 
 void recorddisp(){
-  
+  char consumerName[1000]; // 仮
+  printf("出荷実績を表示します。");
+  printf("顧客名を入力してください。\n");
+  scanf("%c", consumerName);
+
+  if(consumerName == 出荷伝票.名前){
+    // 出荷伝票一覧の表示
+  }
+  else{
+    printf("この名前は存在しません")
+  }
 }
--- program/stockdisp.c
+++ program/stockdisp.c
@@ -1,5 +1,11 @@
 #include "liquorSystem.h"
 
 void stockdisp(){
+  printf("在庫の表示を行います。\n");
+  printf("--------\n");
+
+  // 在庫一覧を全て表示させる。
+
+  printf("--------\n");
   
 }
