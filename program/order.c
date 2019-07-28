206a18ac320ee73935b65403522cededc2b21181
Shusuke Tokutomi <shusuketokutomi@Shusukes-macbookpro.local>
2019-07-29 07:33:25 +0900

    create order

---
+++ program/order.c
@@ -0,0 +1,26 @@
+#include "liquorSystem.h"
+
+void order(){
+  int input_order;
+  printf("プラズマ酒店\n");
+  printf("以下から行いたい項目を選び、1~4 のいずれかを入力してください。\n");
+  printf("1 : 出荷依頼\n");
+  printf("2 : 入荷依頼\n");
+  printf("3 : 在庫表示\n");
+  printf("4 : 出荷実績表示\n");
+  printf("[入力してください：]");
+  scanf("%d", *input_order);
+
+  if (input_order == 1){
+    // メソッド
+  }
+  else if(input_order == 2){
+    // メソッド
+  }
+  else if(input_order == 3){
+    // メソッド
+  }
+  else if(input_order == 4){
+    // メソッド
+  }
+}
