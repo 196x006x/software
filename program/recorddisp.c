20133b3c5b55396164b36fe969d75e92cc3a4574
shusuke-tokutomi <stktmwonderwall703@gmail.com>
2019-07-30 16:53:21 +0900

    入力された顧客が存在しない場合の機能実装

--- program/recorddisp.c
+++ program/recorddisp.c
@@ -6,7 +6,11 @@ void recorddisp(ticket *ticketList){
   printf("顧客名を入力してください。\n");
   scanf("%s", inputName);
 
+  // ticketList内確認用
   int i = 0;
+  // 存在しない場合の確認用
+  int n = 0;
+
   do{
     if(strcmp(ticketList[i].consumerName, inputName) == 0){
       printf("--出荷伝票--\n");
@@ -15,8 +19,16 @@ void recorddisp(ticket *ticketList){
       printf("[お酒の銘柄]%s\n", ticketList[i].liquorName);
       printf("[本数]%d本\n", ticketList[i].numberOfLiquor);
       printf("--------\n");
+      n++;
     }
     i++;
   } while(ticketList[i].consumerName!=NULL);
 
+  if(n==0){
+    printf("!!この名前は存在しません。もう一度入力し直してください。!!\n\n");
+    recorddisp(ticketList);
+  }
+  else{
+    printf("システムを終了します。\n");
+  }
 }
