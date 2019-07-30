b1b4063c45abf5dd2c9624d2130b883f2ebb97ba
Shusuke Tokutomi <shusuketokutomi@Shusukes-macbookpro.local>
2019-07-30 12:24:35 +0900

    修正

--- program/stockdisp.c
+++ program/stockdisp.c
@@ -8,7 +8,7 @@ void stockdisp(liquor *stock){
   int i = 0;
   while(stock[i].liquorName != NULL){
     printf("・%s  ", stock[i].liquorName);
-    printf("%d本", stock[i].numberOfLiquor\n);
+    printf("%d本\n", stock[i].numberOfLiquor);
     i++;
   }
   printf("--------\n");
