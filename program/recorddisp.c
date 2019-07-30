5fd89b70ab23895b92e15922b64dfa15a59cc0d0
shusuke-tokutomi <stktmwonderwall703@gmail.com>
2019-07-30 16:39:21 +0900

    入力と等しい時出荷伝票表示機能の実装

--- program/recorddisp.c
+++ program/recorddisp.c
@@ -1,22 +1,22 @@
 #include "liquorSystem.h"
 
 void recorddisp(ticket *ticketList){
-  ticket ticket;
-  // char *inputName = input_name(ticketList);
   char inputName[1000];
   printf("出荷実績を表示します。");
   printf("顧客名を入力してください。\n");
   scanf("%s", inputName);
 
   int i = 0;
-  while(ticketList[i].consumerName!=NULL){
-    if(ticketList[i].consumerName == inputName){
+  do{
+    if(strcmp(ticketList[i].consumerName, inputName) == 0){
       printf("--出荷伝票--\n");
       printf("[注文番号]%d\n", ticketList[i].orderNumber);
       printf("[名前]%s\n", ticketList[i].consumerName);
       printf("[お酒の銘柄]%s\n", ticketList[i].liquorName);
       printf("[本数]%d本\n", ticketList[i].numberOfLiquor);
+      printf("--------\n");
     }
     i++;
-  }
+  } while(ticketList[i].consumerName!=NULL);
+
 }
